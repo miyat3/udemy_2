bcf3f3a18465939d73084003bd23c70c44da09ad
torumiyake <t.toru.miyake@gmail.com>
2022-07-20 22:31:32 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
 export default function App() {
+  console.log("App");
   const [count, setCount] = useState(0);
 
   const onClickCountUp = () => {
@@ -9,10 +11,11 @@ export default function App() {
   };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <p>{count}</p>
-      <button onClick={onClickCountUp}>カウントアップ</button>
+      <input />
+      <br />
+      <br />
+      <button>表示</button>
+      <ChildArea />
     </div>
   );
 }
